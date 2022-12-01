66c92e91706781d2ba751d49cf0328d0dc62c7d0
KanginaLF <LinaraKangina@gmail.com>
2022-12-01 16:54:39 +0300

    Создала класс Доктор

---
+++ src/main/java/classes/Doctor.java
@@ -0,0 +1,17 @@
+package classes;
+
+import api.DBWoker;
+import api.User;
+
+import java.util.Scanner;
+
+public class Doctor {
+
+    DBWoker worker;
+    Scanner scanner = new Scanner(System.in);
+
+    User user = new User();
+    Patient patient = new Patient();
+
+
+}
