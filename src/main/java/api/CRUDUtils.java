59f2f2e28862db9d8180a5a6eb3fc3a78b54bbd9
KanginaLF <LinaraKangina@gmail.com>
2022-12-01 16:55:51 +0300

    добавила общий метод для поиска карты пациента, возвращает ID пациента

--- src/main/java/api/CRUDUtils.java
+++ src/main/java/api/CRUDUtils.java
@@ -1,13 +1,13 @@
 package api;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import classes.Patient;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CRUDUtils {
+    static Patient patient = new Patient();
 
 
     public static List<User> getUserData(String query)  {
@@ -31,6 +31,37 @@ public class CRUDUtils {
             e.printStackTrace();
         }
         return users;
+    }
+
+    //поиск карты пациента, возвращает ID пациента
+    public static int getPatientCard (String query, String surname, String name, String patronymic, String telephone)  {
+
+        int patientid = 0;
+        try {
+            Connection connection = DBWoker.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, surname);
+            if (name != null) {preparedStatement.setString(2, name);}
+            if (patronymic != null) {preparedStatement.setString(3, patronymic);}
+            if (telephone != null) {preparedStatement.setString(4, telephone);}
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+
+            while (resultSet.next()){
+                if (patientid == 0) {patientid = resultSet.getInt(1);}
+                if (surname == null) { surname  = resultSet.getString(2);}
+                if (name == null) { name  = resultSet.getString(3);}
+                if (patronymic == null)  { patronymic = resultSet.getString(4);}
+                Date birthdate = resultSet.getDate(5);
+                if (telephone == null) telephone = resultSet.getString(6);
+
+
+                System.out.println ("Открыта карточка пациента: " + surname + " " + name + " " + patronymic + ", Дата рождения " + birthdate + ", Телефон " + telephone +"\n");
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return patientid;
     }
 
 //    public static List<User> saveUser (User user, String query){
